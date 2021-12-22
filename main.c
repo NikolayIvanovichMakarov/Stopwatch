06b269978e9268746933b8ab15092f505fc6e14a
nikolay.makarov <nikolay.makarov@eltex.loc>
2021-12-22 14:30:44 +0700

    clean code

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <sys/time.h>
 #include <pthread.h>
 #include <stdint.h>
+#include <ncurses.h>
+
 typedef enum stopwatch_e
 {
 	sw_waiting,
@@ -24,15 +26,10 @@ void start()
 	gettimeofday(&g_start, NULL);
 }
 
-void print_current_time()
-{
-	gettimeofday(&g_stop, NULL);
-	printf("sec: %lu usec: %lu \n", (g_stop.tv_sec - g_start.tv_sec), (g_stop.tv_usec - g_start.tv_usec)); 
-}
-
 uint8_t g_b_tmr_run = 1;
 uint8_t g_b_paused = 0;
 uint8_t g_b_runned = 0;
+uint8_t g_b_cleared = 0;
 
 void * thrd_timer_main_cycle()
 {
@@ -42,11 +39,11 @@ void * thrd_timer_main_cycle()
 		switch(g_current_state)
 		{
 			case sw_waiting:
+				if (g_b_paused == 1)
+					g_b_paused = 0;
 				if (g_b_runned == 1)
 				{
 					g_b_runned = 0;
-					if (g_b_paused == 1)
-						g_b_paused = 0;
 					g_current_state = sw_running;
 					start();
 				}
@@ -62,7 +59,7 @@ void * thrd_timer_main_cycle()
 					sec += usec / 1000000;
 					usec = usec % 1000000;
 				}
-				if (usec < 0)
+				else if (usec < 0)
 				{
 					sec = sec-1;
 					usec = usec + 1000000;
@@ -75,6 +72,17 @@ void * thrd_timer_main_cycle()
 
 					g_current_state = sw_paused;
 				}
+				else if (g_b_cleared == 1)
+				{
+					g_b_cleared = 0;
+					g_current_state = sw_waiting;
+					g_accumulated_time.tv_sec = 0;
+					g_accumulated_time.tv_usec = 0;
+					hour = 0;
+					min = 0;
+					sec = 0;
+					usec = 0;
+				}
 
 				min = sec / 60;
 				hour = min / 60;
@@ -82,12 +90,32 @@ void * thrd_timer_main_cycle()
 				min = min % 60;
 				sec = sec % 60;
 
-				printf("hour: %ld min: %ld sec: %ld usec: %ld \n",hour, min, sec, usec);
+				clear();
+				printw("hour: %ld min: %ld sec: %ld usec: %ld \n",hour, min, sec, usec);
+				refresh();
 				
+				usleep(10000);
 			break;
 			case sw_paused:
 				if (g_b_paused == 1)
 					g_b_paused = 0;
+				if (g_b_cleared == 1)
+				{
+					min = 0;
+					hour = 0;
+					hour = 0;
+					min = 0;
+					sec = 0;
+					usec = 0;
+					g_accumulated_time.tv_usec = 0;
+					g_accumulated_time.tv_sec = 0;
+
+					clear();
+					printw("hour: %ld min: %ld sec: %ld usec: %ld \n",hour, min, sec, usec);
+					refresh();
+
+					g_b_cleared = 0;
+				}
 				if (g_b_runned == 1)
 				{
 					g_b_runned = 0;
@@ -110,9 +138,17 @@ int main()
 	char ch;
 	pthread_create(&thrd_timer, &attr, thrd_timer_main_cycle, NULL);
 	uint8_t main_program = 1;
+
+
+
+	initscr();
+	keypad(stdscr, true); 
+	curs_set(0);
+
 	while(main_program == 1)
 	{
-		scanf("%c",&ch);
+		ch = getch();
+		//scanf("%c",&ch);
 		switch(ch)
 		{
 			case 'q':
@@ -127,10 +163,13 @@ int main()
 			case 'p':
 				g_b_paused = 1;
 			break;
+			case 'c':
+				g_b_cleared = 1;
+			break;
 		}
 	}
 
 
-
+	endwin();
 	return 0;
 }
